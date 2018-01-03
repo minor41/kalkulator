8bc4ec65246d1403a7944d739811a03d14f5c9bc
Piotr Stelmach <minor41@gmail.comgit config --global user.name minor41>
2018-01-03 02:02:27 +0100

    calculator

--- src/main/java/com/kalkulator/Calculator.java
+++ src/main/java/com/kalkulator/Calculator.java
@@ -1,4 +1,20 @@
 package com.kalkulator;
 
 public class Calculator {
+
+    public void addAToB() {
+    }
+
+    public void substractAFromB() {
+    }
+
+    public static void main(String args[]){
+        Calculator calculator = new Calculator();
+
+        calculator.addAToB();
+        System.out.println("It works");
+
+        calculator.substractAFromB();
+        System.out.println("I am happy");
+    }
 }
