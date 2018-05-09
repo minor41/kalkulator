f6415454d43ac4f002767b116d4d6c482a4cf4d7
piotrstelmach <minor41@gmail.com>
2018-05-09 14:38:06 +0200

    spelling check

--- src/main/java/com/kalkulator/Calculator.java
+++ src/main/java/com/kalkulator/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
     public void addAToB() {
     }
 
-    public void substractAFromB() {
+    public void subtractAFromB() {
     }
 
     public static void main(String args[]){
@@ -14,7 +14,7 @@ public class Calculator {
         calculator.addAToB();
         System.out.println("It works");
 
-        calculator.substractAFromB();
+        calculator.subtractAFromB();
         System.out.println("I am happy");
     }
 }
